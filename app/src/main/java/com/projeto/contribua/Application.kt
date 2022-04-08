81d2638109168fc6ff8eee7b605ff1cf8e6f8351
Alysson Henrique Ribeiro <alyssonh92@gmail.com>
2022-04-08 00:12:52 -0300

    Adiciona projeto ao git.

---
+++ app/src/main/java/com/projeto/contribua/Application.kt
@@ -0,0 +1,6 @@
+package com.projeto.contribua
+
+import android.app.Application
+
+class Application : Application() {
+}
---
+++ app/src/main/java/com/projeto/contribua/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.projeto.contribua.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projeto.contribua.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
