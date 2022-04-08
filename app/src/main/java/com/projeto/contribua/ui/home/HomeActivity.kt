eddddde179913a38436983a34fa26810354d3b6c
Alysson Henrique Ribeiro <alyssonh92@gmail.com>
2022-04-08 00:34:45 -0300

    Adiciona configurações padrões.

--- app/src/main/java/com/projeto/contribua/ui/home/HomeActivity.kt
+++ app/src/main/java/com/projeto/contribua/ui/home/HomeActivity.kt
@@ -1,10 +1,11 @@
 package com.projeto.contribua.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.projeto.contribua.R
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
