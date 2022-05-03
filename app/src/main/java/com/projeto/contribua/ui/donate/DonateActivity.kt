8f41c32d10ab5bb2e6299854b9ed5f2c9192ee8d
Alysson Henrique Ribeiro <alyssonh92@gmail.com>
2022-05-03 01:13:14 -0300

    Finalizando funcionalidades da tela de contribuição.

--- app/src/main/java/com/projeto/contribua/ui/donate/DonateActivity.kt
+++ app/src/main/java/com/projeto/contribua/ui/donate/DonateActivity.kt
@@ -1,20 +1,45 @@
 package com.projeto.contribua.ui.donate
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.projeto.contribua.R
 import com.projeto.contribua.databinding.ActivityDonateBinding
 import com.projeto.contribua.extensions.maskMonetary
+import com.projeto.contribua.ui.donate.signature.SignatureActivity
+import com.projeto.contribua.utils.formatdateInString
+import java.util.Calendar
 
-class DonateActivity : AppCompatActivity() {
+class DonateActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityDonateBinding
+    private lateinit var datePickerDialog: DatePickerDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
         setupViews()
+        setupDatePickerDialog()
+    }
+
+    private fun setupDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        datePickerDialog = DatePickerDialog(
+            this,
+            { _, mYear, mMonth, mDay ->
+                binding.buttonContribuationDate.text = formatdateInString(mDay, mMonth, mYear)
+            },
+            year,
+            month,
+            day
+        )
+
     }
 
     override fun onBackPressed() {
@@ -23,10 +48,43 @@ class DonateActivity : AppCompatActivity() {
 
     private fun setupViews() {
         binding.edtValue.maskMonetary()
+        binding.buttonContribuationDate.setOnClickListener(this)
+        binding.buttonSignapure.setOnClickListener(this)
+        binding.buttonFinishContribuation.setOnClickListener(this)
     }
 
     private fun setupBinding() {
         binding = ActivityDonateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onClick(view: View?) {
+        when (view) {
+            binding.buttonContribuationDate -> datePickerDialog.show()
+            binding.buttonSignapure -> startSignatureActivity()
+            binding.buttonFinishContribuation -> validateFields()
+        }
+
+    }
+
+    private fun startSignatureActivity() {
+        val intent = Intent(this, SignatureActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun validateFields() {
+        val nameContributor = binding.edtNameContributor.text.toString()
+        val valueContribuation = binding.edtValue.text.toString()
+        val dateContribuation = binding.buttonContribuationDate.text.toString()
+
+        if (nameContributor.isEmpty() || valueContribuation.isEmpty() || dateContribuation.isEmpty()) {
+            Toast.makeText(
+                this,
+                R.string.input_data_in_all_fields,
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            finish()
+        }
     }
 }
